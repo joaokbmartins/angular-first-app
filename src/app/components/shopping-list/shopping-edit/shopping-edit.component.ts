063e070880c9592bb53053629ab7d1e0c5feeba4
Joao Martins <martins.joao1909@gmail.com>
2020-11-25 01:10:40 -0300

    Adding shopping-list functionalities

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component ({
     selector: 'app-shopping-edit',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
 })
 
 export class ShoppingEditComponent {
+
+  @ViewChild('inputName', { static: true })
+  private inputName: ElementRef = null;
+  
+  @ViewChild('inputAmount', { static: true }) 
+  private inputAmount: ElementRef = null;
+
+  @Output()
+  private addIngredient: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+
+  public onAddIngredient() {
+    const ingredientName: string = this.inputName.nativeElement.value
+    const ingredientAmount:number = this.inputAmount.nativeElement.value;
+    this.addIngredient.emit(new Ingredient(ingredientName, ingredientAmount));
+  }
     
 }
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -6,17 +6,47 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html'
 })
 
-export class ShoppingListComponent implements OnInit{
+export class ShoppingListComponent {
 
   private ingredients: Array<Ingredient> = new Array<Ingredient>();
 
-  constructor() {
+  constructor() {}
 
-  }
+  public addNewIngredient(ingredient:Ingredient) {
+    var existent = false;
+    var found = null;
+    this.ingredients.forEach((ingredientListItem, index) => {
+      if (ingredientListItem.getName() == ingredient.getName()) {
+        found = this.ingredients[index];
+        found.setAmount(
+          (Number(found.getAmount()) + Number(ingredient.getAmount())
+        ));
+        existent = true;
+        return;
+      }    
+    })  
+    if (!existent) {
+      this.ingredients.push(ingredient);
+    }
+  } 
 
-  ngOnInit() {
-    this.ingredients.push(new Ingredient("Apple", 10));
-    this.ingredients.push(new Ingredient("Eggplant", 5));
+  public deleteIngredient(toDelete: Ingredient): void {
+    var existent = false;
+    var found = null;
+    this.ingredients.forEach((ingredientListItem, index) => {
+      if (ingredientListItem.getName() == toDelete.getName()) {
+        found = this.ingredients[index];
+        
+        found.setAmount(
+          (Number(found.getAmount()) + Number(toDelete.getAmount())
+        ));
+        existent = true;
+        return;
+      }    
+    })  
+    if (!existent) {
+      this.ingredients.push(toDelete);
+    }
   }
 
   public getIngredients():Array<Ingredient> {
